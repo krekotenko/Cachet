657724a2d86e94a01fb129afa54ffee3b2bf4d59
Ant <krekotenko@gmail.com>
2019-04-17 23:11:17 +0300

    Change rotation subscribe button

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -5,21 +5,19 @@
 <div @if($appBannerStyleFullWidth)class="app-banner"@endif>
     <div class="container">
         <div class="row app-banner-padding  @if(!$appBannerStyleFullWidth) app-banner @endif">
-                <div class="col-sm-4">
+                <div class="col-sm-12">
                     @if($appDomain)
                     <a href="{{ $appDomain }}" class="links"><img src="data:{{ $appBannerType }};base64, {{ $appBanner }}" class="banner-image img-responsive"></a>
                     @else
                     <img src="data:{{ $appBannerType }};base64, {{ $appBanner }}" class="banner-image img-responsive">
                     @endif
-                </div>
-            @if(Request::url() === cachet_route('subscribe'))
-                <div class="col-sm-8"><p><a href="{{ cachet_route('status-page') }}" class="btn btn-success btn-outline  pull-right"><i class="ion ion-home"></i></a></p></div>
-            @else
-                @if($enableSubscribers)
-                    <div class="col-sm-8">
+                    @if(Request::url() === cachet_route('subscribe'))
+                        <a href="{{ cachet_route('status-page') }}" class="btn btn-success btn-outline pull-right"><i class="ion ion-home"></i></a></p>
+                    @else
+                    @if($enableSubscribers)
                         <a class="btn btn-success btn-outline pull-right" href="{{ cachet_route('subscribe') }}">{{ trans('cachet.subscriber.button') }}</a>
-                    </div>
-                @endif
+                    @endif
+                </div>
             @endif
         </div>
     </div>
